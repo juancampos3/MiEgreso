b4ddffadb76804c179624611623602a53817d472
juancampos3 <jucampos3@poligran.edu.co>
2022-01-12 15:28:21 -0500

    Creación y Configuración de Barra de Navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,20 @@
     href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
   </head>
   <body class="bg-gray-100 text-gray-800">
-    <!-- Document body -->
+    <nav class="flex py-5 bg-indigo-500 text-white">
+        <div class="w-1/2 px-12 mr-auto">
+            <p class="text-2xl font-bold">Mi Egreso</p>
+        </div>
+
+        <ul class="w-1/2 px-16 ml-auto flex justify-end pt-1">
+            <li class="mx-2">
+                <a href="{{route('login.index')}}" class="fond-semibold hover:bg-indigo-700 py-3 px-4 rounded-md">Login</a>
+            </li>
+            <li>
+                <a href="{{route('register.index')}}" class="font-semibold border-2 border-white py-2 px-4 rounded-md hover:bg-white hover:text-indigo-700">Register</a>
+            </li>
+        </ul>
+    </nav>
 
 
     @yield('content')

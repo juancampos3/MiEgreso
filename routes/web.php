6677e131fa35f11d36d0830d45e8e77a31a36ab7
juancampos3 <jucampos3@poligran.edu.co>
2022-01-12 13:14:20 -0500

    Agregar Rutas de Controladores

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
 /*
 |--------------------------------------------------------------------------

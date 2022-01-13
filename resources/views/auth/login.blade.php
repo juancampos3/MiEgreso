eccda85c4a18cc1c212aa7114cce15958baa11d4
juancampos3 <jucampos3@poligran.edu.co>
2022-01-12 19:09:59 -0500

    Almacenar Información de Registro en Base de Datos

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,7 @@
     <div class="block mx-auto my-12 p-8 bg-white w-1/3 border border-gray-200 rounded-lg shadow-lg">
         <h1 class="text-3xl text-center font-bold">Inicie Sesion</h1>
         <form class="mt-4" method="POST" action="">
+            @csrf
             <input type="email" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Ingrese su Email" id="email" name="email">
             <input type="password" class="border border-gray-200 rounded-md bg-gray-200 w-full text-lg placeholder-gray-900 p-2 my-2 focus:bg-white" placeholder="Ingrese su Contraseña" id="password" name="password">
 
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ Route::get('/', function () {
 });
 
 Route::get('/register', [RegisterController::class, 'create'])->name('register.index');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
 Route::get('/login', [SessionsController::class, 'create'])->name('login.index');
